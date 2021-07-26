7b243a525dd1ef883222ae04d3d5748e4035357b
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-07-27 04:09:43 +0530

    ADD:Implementation of stack using Array

---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+        Stack stack=new Stack();
+         stack.push(56);
+        stack.push(30);
+        stack.push(70);
+        stack.show();
+    }
+}
